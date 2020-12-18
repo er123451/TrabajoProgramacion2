bbaf0e3d8d563dcd8807dc11bcf52600419e6472
er123451 <47930798+er123451@users.noreply.github.com>
2020-12-18 13:44:13 +0100

    Add files via upload

---
+++ Trabajo2Programacion/src/trabajo2/FueraDeLimitesException.java
@@ -0,0 +1,8 @@
+package trabajo2;
+
+public class FueraDeLimitesException extends Exception{
+
+	public FueraDeLimitesException() {
+		System.err.println("Fuera de rango");
+	}
+}
--- Trabajo2Programacion/src/trabajo2/Lista.java
+++ Trabajo2Programacion/src/trabajo2/Lista.java
@@ -24,10 +24,43 @@ public class Lista {
 	}
 	
 	public void Concatenar(Lista in){
-		int size = list.length+in.Tamano();
-		double 
+		for (int i = 0;i<list.length;i++) {
+			Anadir(in.Get(i));
+		}
 	}
 	
+	public void borrar(char e) {
+		for (int i = 0;i<list.length;i++) {
+			if (list[i] == e) {
+				Rm(i);
+			}
+		}
+	}
+	
+	public char Get(int i) throws FueraDeLimitesException{
+		if (i>= list.length){
+			throw new FueraDeLimitesException();
+		}
+		return list[i];
+	}
+	
+	public boolean EstaVacia() {
+		if (pos == 0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public boolean Contiene(char e) {
+		boolean test = false;
+		for (int i = 0;i<list.length;i++) {
+			if (list[i] == e) {
+				test = true;
+			}
+		}
+		return test;
+	}
 //metodos auxiliares de la clase (private)
 	
 	private void Resize(int size){
@@ -38,4 +71,14 @@ public class Lista {
 		}
 	}
 	
+	private void Rm(int p) {
+		char[] buffer = new char[list.length];
+		int j = 0;
+		for (int i = 0; i<list.length;i++) {
+			try {
+				if(i == p) i++;
+				buffer[j++] = list[i];
+			}catch(Exception e) {}
+		}
+	}
 }
