600a8ff03dd0bf30043367939e1cc9b6066b3bfa
Diser07 <76215190+Diser07@users.noreply.github.com>
2020-12-29 19:28:18 +0100

    Add files via upload

---
+++ Trabajo2Programacion/src/trabajo2/ColaLlenaExcpetion.java
@@ -0,0 +1,13 @@
+package trabajo2;
+
+
+	public class ColaLlenaExcpetion extends  Exception
+	{
+		public ColaLlenaException () 
+		{
+			System.err.println("No hay hueco en el array dado que está lleno");
+			
+		}
+	
+
+	}
