5f1aaa296415f421fe79bc96830e34c459cc4fa7
Diser07 <76215190+Diser07@users.noreply.github.com>
2020-12-29 19:27:47 +0100

    Add files via upload

---
+++ Trabajo2Programacion/src/trabajo2/ColaCircular.java
@@ -0,0 +1,108 @@
+
+public class ColaCircular 
+{
+	char [] colacir;
+	int pos;
+
+
+
+
+	public ColaCircular (int size) 
+	{
+		colacir = new char[size];
+	    pos=0;
+	
+	}
+	public ColaCircular()
+	{
+		colacir = new char [10];
+		pos = 0;
+	}
+	
+	public int tamano()
+	{
+		return pos;
+	}
+
+	public char primero() 
+	{
+		if (i=colacir.length) 
+		{
+			throw new ColaVaciaException();
+			
+		}
+		return colacir[1];
+	}
+	
+	
+	
+
+	public void añadir (char e)
+	{
+		if (this.colacir.length<this.pos+1)
+		{
+			throw new ColaLlenaExcpetion();
+		}
+		colacir [pos++]=e;
+	
+	
+	}
+
+	public char extraer () 
+	{
+		if(this.colacir.length<this.pos-1)
+		{
+			throw new ColaVaciaExcpetion();
+		}
+		
+		colacir[pos--]=e;
+	}
+	
+	
+	
+	public boolean estaVacia()
+	{
+		if (pos==0) {
+			return true;
+		}else 
+		{
+			return false;
+		}
+	}
+		
+	
+	
+	public boolean estaLlena()
+	{
+		if (pos==10) {
+			return true;
+			
+		}else
+		{
+			return false;
+		}
+	}
+		
+	
+
+	public boolean contiene (char e)
+	{
+		boolean test = false;
+		for(int i=0;i<colacir.length;i++)
+		{
+			if (colacir[i]==e)
+			{
+				test=true;
+			}
+			
+		}
+		return test;
+	}
+	
+	
+
+	public void mostrar () 
+	{
+	
+	}
+
